a32870dbc2e05fd63f517cf0d1f3f01f87da88be
Shawn <shawn.nigel@gmail.com>
2016-07-31 19:55:04 +0530

    Added Flash notices and styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
+  
    if params[:site]=="Digg" then
+   	flash[:success]="Returned results from Digg"
     @pages=Scrape.new("http://digg.com/search?q=welcome")
 	@pages=@pages.get_page
 	@form=@pages.form_with(:id=>"search-page-form")
@@ -9,6 +11,9 @@ class StaticPagesController < ApplicationController
 
 	@titles=@result.search("h2 a")
    else
+   	if params[:site]=="Reddit"
+   	  flash[:success]="Returned results from Reddit"
+   	 end
    	 @pages=Scrape.new("http://reddit.com")
   	 @pages=@pages.get_page
   	 @form=@pages.form_with(:action=> "https://www.reddit.com/search")
