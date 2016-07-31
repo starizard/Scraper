dcebf4781f1815e6b0f0edd191fc33d5b64c9595
Shawn <shawn.nigel@gmail.com>
2016-07-31 18:58:40 +0530

    Added Digg ,RIP Digg

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,21 @@
 class StaticPagesController < ApplicationController
   def index
-  	@pages=Scrape.new("http://reddit.com")
-  	@pages=@pages.get_page
-  	@form=@pages.form_with(:action=> "https://www.reddit.com/search")
-  	@form.fields.first.value=params[:search]
-  	@result=@form.click_button
+   if params[:site]=="Digg" then
+    @pages=Scrape.new("http://digg.com/search?q=welcome")
+	@pages=@pages.get_page
+	@form=@pages.form_with(:id=>"search-page-form")
+	@form.fields.first.value=params[:search]
+	@result=@form.click_button
 
-  	@titles=@result.search(".search-result-link .search-title")
-  
+	@titles=@result.search("h2 a")
+   else
+   	 @pages=Scrape.new("http://reddit.com")
+  	 @pages=@pages.get_page
+  	 @form=@pages.form_with(:action=> "https://www.reddit.com/search")
+  	 @form.fields.first.value=params[:search]
+  	 @result=@form.click_button
+     @titles=@result.search(".search-result-link .search-title")
+   end
   end
 
   def help
